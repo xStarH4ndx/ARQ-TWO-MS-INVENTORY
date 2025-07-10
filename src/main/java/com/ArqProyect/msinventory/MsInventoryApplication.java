86db126c9c5b7c3889a768929d2f2ad120070fd9
xStarH4ndx <brunotoro1799@gmail.com>
2025-07-10 11:35:02 -0400

    listo

--- src/main/java/com/ArqProyect/msinventory/MsInventoryApplication.java
+++ src/main/java/com/ArqProyect/msinventory/MsInventoryApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableRabbit
 @SpringBootApplication
 public class MsInventoryApplication {
-
 	public static void main(String[] args) {
-		System.out.println("MONGODB_URI = " + System.getenv("MONGODB_URI"));
-		System.out.println("RABBITMQ_URI = " + System.getenv("RABBITMQ_URI"));
 		SpringApplication.run(MsInventoryApplication.class, args);
 	}
 
